a28951ea11858271d25e4d2125f6e35da1296edb
vaishnavi <vaishnavidkhose@gmail.com>
2023-05-30 06:52:41 +0530

    updated

--- rmi/AddServerImpl.java
+++ rmi/AddServerImpl.java
@@ -4,7 +4,7 @@ import java.rmi.server.*;
 public class AddServerImpl extends UnicastRemoteObject implements AddServerIntf {
 
     public AddServerImpl() throws RemoteException {}
-
+    // i have added a comment
     @Override
     public double add(double d1, double d2) throws RemoteException {        
         return (d1 + d2);
